9512c35122d7df7f97b8878a76aebd73a429974a
Faizan Ahmad Khattak <155997339+faiziktk@users.noreply.github.com>
2026-08-11 17:19:16 +0100

    Set minimum exponent limit for fp16 and binary16
    Add minimum exponent limit for product alignment in TC.

--- models/H100TC.m
+++ models/H100TC.m
@@ -96,6 +96,8 @@ if ismember(informat, {'fp16','half','binary16'})
     if exist('outformat', 'var')
         if ismember(outformat, {'fp16','binary16','half'})
             def_params.frmode='rne'; % TC final rounding mode
+            def_params.min_exp_limit   = -15; % Minimum exponent allowed for product alignment
+
         end  
     end
 elseif ismember(informat, {'tf32', 'tensorfloat32'})
